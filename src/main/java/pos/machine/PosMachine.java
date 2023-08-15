7a239335bd7c3acb8f7f9ed9c32d96ff7e7b8fd7
Elaine De los Reyes <elaine.leah.marie.de.los.reyes@oocl.com>
2023-08-15 22:38:11 +0800

    feat: created getItemInfo method

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,29 @@
 package pos.machine;
 
-import java.util.List;
+import java.util.*;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
+        Map<String, List<Item>> itemInfo = getItemInfo(barcodes);
         return null;
+    }
+
+    private Map<String, List<Item>> getItemInfo(List<String> barcodes){
+        List<Item> items = ItemsLoader.loadAllItems();
+        Map<String, List<Item>> itemsCache = new HashMap<>();
+        barcodes.forEach(barcode -> {
+            Optional<Item> filteredItem = items.stream().filter(item -> item.getBarcode().equals(barcode)).findFirst();
+            if(filteredItem.isPresent()) {
+                List<Item> finalFilteredItems = itemsCache.get(barcode);
+                if (finalFilteredItems == null) {
+                    finalFilteredItems = new ArrayList<>();
+                    finalFilteredItems.add(filteredItem.get());
+                } else {
+                    finalFilteredItems.add(filteredItem.get());
+                }
+                itemsCache.put(barcode, finalFilteredItems);
+            }
+        });
+        return itemsCache;
     }
 }
